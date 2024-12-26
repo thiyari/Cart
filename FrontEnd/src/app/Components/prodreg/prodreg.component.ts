556b3fb746e7905f3f8a60aec15edb92ec31e051
thiyari <manikanth578@gmail.com>
2024-12-26 19:30:45 +0530

    header update

---
+++ FrontEnd/src/app/Components/prodreg/prodreg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodreg',
+  standalone: false,
+  
+  templateUrl: './prodreg.component.html',
+  styleUrl: './prodreg.component.scss'
+})
+export class ProdregComponent {
+
+}
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdregComponent } from './Components/prodreg/prodreg.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { ProductsComponent } from './Components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdregComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
