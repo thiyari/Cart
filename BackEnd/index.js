be2edef04a53139f187a997cd8a0bb0bafe6c5ec
thiyari <manikanth578@gmail.com>
2024-12-27 22:57:31 +0530

    update urlencoding

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,7 +11,7 @@ app.use(cors(
    
   ));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 const checkDB = async()=>{
     try{
         // mongodb connection string
