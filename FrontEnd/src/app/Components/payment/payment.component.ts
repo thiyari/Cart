f9b5ad7c84d99b6389747b7227367db635fdc97f
thiyari <manikanth578@gmail.com>
2025-01-13 21:17:49 +0530

    razorpay integraiton update

--- FrontEnd/src/app/Components/payment/payment.component.ts
+++ FrontEnd/src/app/Components/payment/payment.component.ts
@@ -2,22 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../service/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { LocalService } from '../../service/local.service';
+import { WindowRefService } from '../../window-ref.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-payment',
   standalone: false,
   
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.scss'
+  styleUrl: './payment.component.scss',
+  providers: [WindowRefService]
 })
 export class PaymentComponent implements OnInit{
 
   public orders: any;
+  Razorpay: any;
 
   constructor(
     private api: ApiService, 
     private route: ActivatedRoute,
-    private localStore: LocalService
+    private localStore: LocalService,
+    private winRef: WindowRefService
   ){}
 
   ngOnInit(): void {
@@ -43,5 +48,39 @@ export class PaymentComponent implements OnInit{
       merchantTransactionID: this.orders.referenceid
     }
     this.api.phonepe(data)
+  }
+  razorpay_payment(){
+    const RozarpayOptions = {
+      description: 'Sample Razorpay demo',
+      currency: 'INR',
+      amount: this.orders.grandtotal*100,
+      name: this.orders.name,
+      key: environment.RAZOR_PAY_KEY,
+      image: 'https://i.imgur.com/FApqk3D.jpeg',
+      prefill: {
+        name: 'sai kumar',
+        email: 'sai@gmail.com',
+        phone: '9898989898'
+      },
+      theme: {
+        color: '#6466e3'
+      },
+      modal: {
+        ondismiss:  () => {
+          console.log('dismissed')
+        }
+      }
+    }
+
+    const successCallback = (paymentid: any) => {
+      console.log(paymentid);
+    }
+
+    const failureCallback = (e: any) => {
+      console.log(e);
+    }
+
+    const rzp = new this.winRef.nativeWindow.Razorpay(RozarpayOptions, successCallback, failureCallback);
+    rzp.open();
   }
 }
