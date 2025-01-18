27530159ad3af3fb297c44b77c43baaa114c1921
thiyari <manikanth578@gmail.com>
2025-01-19 02:32:10 +0530

    update

--- FrontEnd/src/app/Components/user-orders/user-orders.component.ts
+++ FrontEnd/src/app/Components/user-orders/user-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../service/api.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-user-orders',
@@ -10,16 +11,31 @@ import { ApiService } from '../../service/api.service';
 })
 export class UserOrdersComponent implements OnInit {
 
-  orders: any;
-  response: any;
-  transaction: any;
+  reference_ids: Set<number> = new Set();
+  orders_records: any;
+  paypal_records:any[] = [];
   constructor(private api: ApiService){}
 
   ngOnInit(): void {
-
     const mail_id = "manikanth578@gmail.com"; 
+    this.api.getOrders()
+    .subscribe(res=>{
+      if (res.message === "Success"){
+        this.orders_records = res.records.filter((item:any)=>item.email===mail_id)
+        this.orders_records.map((x:any)=>this.reference_ids.add(x.referenceid))
+        }
+      })
+
+
+      this.api.paypal_txn()
+        .subscribe(res=>{
+          if (res.message === "Success"){
+              this.paypal_records = res.records.filter((x:any)=>x.referenceid === this.reference_ids.forEach((e:any)=>e))
+            }
+          });
+          console.log(this.paypal_records)
 
   }
-    
+  
 
 }
