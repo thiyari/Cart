f103d01d2c8212c568fd62a73eab09793bd62110
thiyari <manikanth578@gmail.com>
2025-01-20 17:11:51 +0530

    update

--- FrontEnd/src/app/Components/view-request/view-request.component.ts
+++ FrontEnd/src/app/Components/view-request/view-request.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../service/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-request',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './view-request.component.html',
   styleUrl: './view-request.component.scss'
 })
-export class ViewRequestComponent {
+export class ViewRequestComponent implements OnInit{
+  public order:any;
+  constructor(
+        private api: ApiService,
+        private route: ActivatedRoute,
+  ){}
 
+  ngOnInit(): void {
+    const order_id = this.route.snapshot.params['orderid'];
+    this.api.getOrders()
+    .subscribe(res=>{
+      if (res.message === "Success"){
+        this.order = res.records.find((item:any)=>JSON.stringify(item.orderid)===order_id)
+        }
+      })
+  }
 }
