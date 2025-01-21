87010fabd4842e6bddf06d8677b5159459df0863
thiyari <manikanth578@gmail.com>
2025-01-21 19:21:27 +0530

    update aggregation service

---
+++ FrontEnd/src/app/service/aggregation.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AggregationService {
+
+  orders_records: any;
+  reference_ids:any[] = [];
+  paypal_records:any[] = [];
+  razorpay_records: any[] = [];
+  phonepe_records: any[] = [];
+  googlepay_records: any[] = []
+  aggregation: any[] = []
+
+  constructor(private api: ApiService) { }
+
+
+  merge(mail_id: any){
+
+    this.api.getOrders()
+    .subscribe(res=>{
+      if (res.message === "Success"){
+        this.orders_records = res.records.filter((item:any)=>item.email===mail_id)
+        this.orders_records.map((x:any)=>this.reference_ids.push(x.referenceid))
+        }
+      })
+
+    this.api.paypal_txn()
+      .subscribe(res=>{
+        if (res.message === "Success"){
+          for (let i = 0; i < this.reference_ids.length; i++){
+            res.records.map((item:any)=>{
+              if(item.referenceid === this.reference_ids[i]){
+                this.paypal_records.push(item)
+              }
+            })
+          }
+          }
+        });
+
+
+    this.api.razorpay_txn()
+        .subscribe(res=>{
+          if (res.message === "Success"){
+            for (let i = 0; i < this.reference_ids.length; i++){
+              res.records.map((item:any)=>{
+                if(item.referenceid === this.reference_ids[i]){
+                  this.razorpay_records.push(item)
+                }
+              })
+            }
+            }
+          });
+
+    this.api.phonepe_txn()
+          .subscribe(res=>{
+            if (res.message === "Success"){
+              for (let i = 0; i < this.reference_ids.length; i++){
+                res.records.map((item:any)=>{
+                  if(item.referenceid === this.reference_ids[i]){
+                    this.phonepe_records.push(item)
+                  }
+                })
+              }
+              }
+            });
+
+    this.api.googlepay_txn()
+            .subscribe(res=>{
+              if (res.message === "Success"){
+                for (let i = 0; i < this.reference_ids.length; i++){
+                  res.records.map((item:any)=>{
+                    if(item.referenceid === this.reference_ids[i]){
+                      this.googlepay_records.push(item)
+                    }
+                  })
+                }
+                }
+              });
+  
+
+      let mergedPaypal = this.paypal_records.map((paypal:any) => {
+          let ordersArray = this.orders_records.find((orders:any) => orders.referenceid === paypal.referenceid);
+          return Object.assign({}, ordersArray, paypal);
+      });
+
+
+      let mergedRazorpay = this.razorpay_records.map((razorpay:any) => {
+        let ordersArray = this.orders_records.find((orders:any) => orders.referenceid === razorpay.referenceid);
+        return Object.assign({}, ordersArray, razorpay);
+      });      
+
+      let mergedPhonepe = this.phonepe_records.map((phonepe:any) => {
+        let ordersArray = this.orders_records.find((orders:any) => orders.referenceid === phonepe.referenceid);
+        return Object.assign({}, ordersArray, phonepe);
+      });    
+
+      
+      let mergedGooglepay = this.googlepay_records.map((googlepay:any) => {
+        let ordersArray = this.orders_records.find((orders:any) => orders.referenceid === googlepay.referenceid);
+        return Object.assign({}, ordersArray, googlepay);
+      });
+
+      this.aggregation = [...mergedPaypal,...mergedRazorpay,...mergedPhonepe,...mergedGooglepay]
+      return this.aggregation
+  }
+}
