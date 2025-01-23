3420537aa51262339d5eee403454fd2908e2b89f
thiyari <manikanth578@gmail.com>
2025-01-23 22:16:05 +0530

    update

--- FrontEnd/src/app/Components/view-order/view-order.component.ts
+++ FrontEnd/src/app/Components/view-order/view-order.component.ts
@@ -94,7 +94,7 @@ export class ViewOrderComponent implements OnInit{
           symbol: '$',
           fractionalUnit: {
             name: 'Cent',
-            plural: 'Cent',
+            plural: 'Cents',
             symbol: '',
           },
         },
