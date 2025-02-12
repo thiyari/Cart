c01a80d548e78b7e6f8f25d2bbfb094b4b17f204
thiyari <manikanth578@gmail.com>
2025-02-12 22:46:29 +0530

    update

--- FrontEnd/src/app/Components/prodreg/prodreg.component.ts
+++ FrontEnd/src/app/Components/prodreg/prodreg.component.ts
@@ -38,7 +38,6 @@ export class ProdregComponent implements OnInit {
 
   upload_images(event:any){
     let base64:string[] = []
-    this.files.map((file:any)=>{console.log(file.name)})
     for (let i = 0; i < event.target.files.length; i++) {
        this.files.push(event.target.files[i].name)
        let file = event.target.files[i];
