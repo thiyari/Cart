a81a070caa9dcd5003268a7a3a570b70611f2009
thiyari <manikanth578@gmail.com>
2025-02-13 17:02:04 +0530

    update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -20,7 +20,7 @@ app.use(cors(
    
   ));
 
-app.use(express.json({limit: "500mb"})); // setting limit to 50 MB (52428800 in bytes)
+app.use(express.json({limit: "500mb"})); 
 app.use(express.urlencoded({limit: "500mb", extended: true , parameterLimit: 100000}));
   
 
