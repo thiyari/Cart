0b10ac13583ebcf52bd2a7883354bf05fb37f04e
thiyari <manikanth578@gmail.com>
2025-02-13 15:23:17 +0530

    update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -39,9 +39,8 @@ app.use(session({
 ))
 
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true }));
-app.use(express.text({ limit: '50mb' }));
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb', extended: true }));
 
 //app.use(bodyParser.json({limit: '50mb'}));
 //app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
