280f08000ec36fbd9e3ce2e0c0adb528f798b569
thiyari <manikanth578@gmail.com>
2025-02-25 15:25:52 +0530

    update

--- FrontEnd/src/app/Components/dollar-factor/dollar-factor.component.ts
+++ FrontEnd/src/app/Components/dollar-factor/dollar-factor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../service/api.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dollar-factor',
@@ -11,13 +12,31 @@ import { environment } from '../../../environments/environment';
   styleUrl: './dollar-factor.component.scss'
 })
 export class DollarFactorComponent implements OnInit{
-
+  dollar_factor: any;
+  toggle: boolean = false;
   constructor(     
     private api: ApiService,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ){}
   ngOnInit(): void {
+    this.http.get<any>(`${environment.SERVER_URI}/api/session`)
+    .subscribe((res)=>{
+          if(res.valid){
+              if (res.log_status === "admin") {
+                // to do
+            }
+          } else {
+            this.router.navigate(['/login'])
+          }
+    })
+  }
 
+  onEdit(){
+    this.toggle = true;
+  }
+  onSubmit(){
+    this.toggle = false;
   }
 
   logout(){
