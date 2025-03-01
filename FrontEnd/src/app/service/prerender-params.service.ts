a2e68884cf56cf3f82d211f2fc4bfe8cf5be0a3b
thiyari <manikanth578@gmail.com>
2025-03-02 00:09:09 +0530

    update

--- FrontEnd/src/app/service/prerender-params.service.ts
+++ FrontEnd/src/app/service/prerender-params.service.ts
@@ -21,7 +21,7 @@ export class PrerenderParamsService {
   edit_admin(){
     this.api.getAdmins().subscribe(res=>{
       if (res.message === "Success"){
-            res.records.map((x:any)=>(this.admin_ids.push({pid: JSON.stringify(x.pid)})))
+            res.records.map((x:any)=>this.admin_ids.push({pid: JSON.stringify(x.pid)}))
       }
     })
     return Promise.resolve(this.admin_ids)
@@ -30,7 +30,7 @@ export class PrerenderParamsService {
   product() {
     this.api.getProducts().subscribe(res=>{
       if (res.message === "Success"){
-            res.records.map((x:any)=>(this.product_ids.push({id: JSON.stringify(x.pid)})))
+            res.records.map((x:any)=>this.product_ids.push({id: JSON.stringify(x.pid)}))
         }
     })
     return Promise.resolve(this.product_ids)
@@ -39,7 +39,7 @@ export class PrerenderParamsService {
  product_view() {
   this.api.getProducts().subscribe(res=>{
     if (res.message === "Success"){
-          res.records.map((x:any)=>(this.product_ids.push({pid: JSON.stringify(x.pid)})))
+          res.records.map((x:any)=>this.product_ids.push({pid: JSON.stringify(x.pid)}))
       }
   })
   return Promise.resolve(this.product_ids)
@@ -48,7 +48,7 @@ export class PrerenderParamsService {
   orders() {
     this.api.getOrders().subscribe(res=>{
       if (res.message === "Success"){
-          res.records.map((x:any)=>(this.order_ids.push({orderid: JSON.stringify(x.orderid)})))
+          res.records.map((x:any)=>this.order_ids.push({orderid: JSON.stringify(x.orderid)}))
       }
     })
     return Promise.resolve(this.order_ids)
@@ -57,7 +57,7 @@ export class PrerenderParamsService {
   phonepe_txn(){
     this.api.phonepe_txn().subscribe(res=>{
       if (res.message === "Success"){
-        res.records.map((x:any)=>(this.phonepe_txn_ids.push({referenceid: JSON.stringify(x.referenceid)})))
+        res.records.map((x:any)=>this.phonepe_txn_ids.push({referenceid: JSON.stringify(x.referenceid)}))
       }
     })
     return Promise.resolve(this.phonepe_txn_ids)
@@ -66,7 +66,7 @@ export class PrerenderParamsService {
   googlepay_txn(){
     this.api.googlepay_txn().subscribe(res=>{
       if (res.message === "Success"){
-        res.records.map((x:any)=>(this.googlepay_txn_ids.push({referenceid: JSON.stringify(x.referenceid)})))
+        res.records.map((x:any)=>this.googlepay_txn_ids.push({referenceid: JSON.stringify(x.referenceid)}))
       }
     })
     return Promise.resolve(this.googlepay_txn_ids)
@@ -75,7 +75,7 @@ export class PrerenderParamsService {
   razorpay_txn(){
     this.api.razorpay_txn().subscribe(res=>{
       if (res.message === "Success"){
-          res.records.map((x:any)=>(this.razorpay_txn_ids.push({referenceid: JSON.stringify(x.referenceid)})))
+          res.records.map((x:any)=>this.razorpay_txn_ids.push({referenceid: JSON.stringify(x.referenceid)}))
       }
     })
     return Promise.resolve(this.razorpay_txn_ids)
@@ -84,14 +84,14 @@ export class PrerenderParamsService {
   paypal_txn(){
     this.api.paypal_txn().subscribe(res=>{
       if (res.message === "Success"){
-          res.records.map((x:any)=>(this.paypal_txn_ids.push({referenceid: JSON.stringify(x.referenceid)})))
+          res.records.map((x:any)=>this.paypal_txn_ids.push({referenceid: JSON.stringify(x.referenceid)}))
       }
     })
     return Promise.resolve(this.paypal_txn_ids)
   }
 
   payment_txn(){
-    this.transactions.merge_admindata().map((x:any)=>(this.payment_referenceids.push({referenceid: JSON.stringify(x.referenceid)})))
+    this.transactions.merge_admindata().map((x:any)=>this.payment_referenceids.push({referenceid: JSON.stringify(x.referenceid)}))
     return Promise.resolve(this.payment_referenceids)
   }
 }
